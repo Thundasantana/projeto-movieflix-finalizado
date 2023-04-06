c39565cdc31d3a79a27fdb4d5af58b4106fa8c7a
Ednilson Santana <ednilson.sdas@outlook.com>
2023-04-06 18:11:36 -0300

    MovieCard

--- frontweb/src/pages/Private/Movies/ListMovie/index.tsx
+++ frontweb/src/pages/Private/Movies/ListMovie/index.tsx
@@ -10,14 +10,16 @@ const ListMovie = () => {
     "year": 2020,
     "imgUrl": "https://image.tmdb.org/t/p/w533_and_h300_bestv2/wu1uilmhM4TdluKi2ytfz8gidHf.jpg",
     "synopsis": "Onde está Gary? Segundo Bob Esponja, Gary foi \"caracolstrado\" pelo temível Rei Poseidon e levado para a cidade perdida de Atlantic City. Junto a Patrick Estrela, ele sai em uma missão de resgate ao querido amigo, e nesta jornada os dois vão conhecer novos personagens e viver inimagináveis aventuras.",
-    "genre": {
+    "genre": [{
         "id": 1,
         "name": "Comédia"
-    }
+    }]
   }
   
   return (
     <div>
+      <MovieCrudCard movie={movie} />
+      <MovieCrudCard movie={movie} />
       <MovieCrudCard movie={movie} />
     </div>
   );
--- frontweb/src/pages/Private/Movies/index.tsx
+++ frontweb/src/pages/Private/Movies/index.tsx
@@ -1,13 +1,21 @@
-import { Route, Switch } from 'react-router-dom';
-import ListMovie from './ListMovie';
+import { Link } from 'react-router-dom';
+import './styles.css';
 
 const Movies = () => {
   return (
-    <Switch>
-      <Route path="/private/movies" exact>
-        <ListMovie />
-      </Route>
-    </Switch>
+    <div className="movie-container">
+      <h1>Tela listagem de filmes</h1>
+      <div className="movie-list">
+        <ul>
+          <Link to="/movies/1">
+            <li>Acessar /movies/1</li>
+          </Link>
+          <Link to="/movies/2">
+            <li>Acessar /movies/2</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
   );
 };
 
