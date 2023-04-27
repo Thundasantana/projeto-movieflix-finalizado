a1c1fae8fec4960f4d6dfaaf97fa24ce02c61035
Ednilson Santana <ednilson.sdas@outlook.com>
2023-04-27 20:23:36 -0300

    ProductDetails loaders

--- frontweb/src/pages/Private/MovieDetails/index.tsx
+++ frontweb/src/pages/Private/MovieDetails/index.tsx
@@ -2,25 +2,33 @@ import { useEffect, useState } from 'react';
 import { BASE_URL } from 'util/requests';
 import axios from 'axios';
 import { Movie } from 'type/movie';
+import MovieInfoLoader from './MovieInfoLoader';
+import MovieDetailsLoader from './MovieDetailsLoader';
 
 import './styles.css';
 import { useParams } from 'react-router-dom';
 
 type UrlParams = {
   movieId: string;
-}
+};
 
 const MovieDetails = () => {
+  const { movieId } = useParams<UrlParams>();
 
-const { movieId } = useParams<UrlParams>();
+  const [isLoading, setIsLoading] = useState(false);
 
   const [movie, setMovie] = useState<Movie>();
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/movies/${movieId}`)
-    .then((response) => {
-      setMovie(response.data);
-    });
+    setIsLoading(true);
+    axios
+      .get(`${BASE_URL}/movies/${movieId}`)
+      .then((response) => {
+        setMovie(response.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, [movieId]);
 
   return (
@@ -28,21 +36,30 @@ const { movieId } = useParams<UrlParams>();
       <div className=" base-card movie-details-card">
         <div className="row">
           <div className="col-xl-6">
-            <div className="img-container">
-              <img src={movie?.imgUrl}
-               alt={movie?.title} />
-            </div>
+            {isLoading ? (
+              <MovieInfoLoader />
+            ) : (
+              <>
+                <div className="img-container">
+                  <img src={movie?.imgUrl} alt={movie?.title} />
+                </div>
 
-            <div className="title-year-subtitle-container">
-              <h2>{movie?.title}</h2>
-              <h4>{movie?.year}</h4>
-              <p>{movie?.subTitle}</p>
-            </div>
+                <div className="title-year-subtitle-container">
+                  <h2>{movie?.title}</h2>
+                  <h4>{movie?.year}</h4>
+                  <p>{movie?.subTitle}</p>
+                </div>
+              </>
+            )}
           </div>
           <div className="col-xl-6">
-            <div className="description-container">
-              <p>{movie?.synopsis}</p>
-            </div>
+            {isLoading ? (
+              <MovieDetailsLoader />
+            ) : (
+              <div className="description-container">
+                <p>{movie?.synopsis}</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
