9b9e16c692f5b445e2b742c7b846de226a3fedc0
Ednilson Santana <ednilson.sdas@outlook.com>
2023-04-28 09:29:56 -0300

    MovieDetails hit

--- frontweb/src/pages/Private/MovieDetails/index.tsx
+++ frontweb/src/pages/Private/MovieDetails/index.tsx
@@ -1,12 +1,14 @@
+import { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
-import { BASE_URL } from 'util/requests';
-import axios from 'axios';
+import { useParams } from 'react-router-dom';
 import { Movie } from 'type/movie';
-import MovieInfoLoader from './MovieInfoLoader';
-import MovieDetailsLoader from './MovieDetailsLoader';
+import { requestBackend } from 'util/requests';
+import { Review } from 'type/review';
+import ReviewForm from 'components/ReviewForm';
+import ReviewListing from 'components/ReviewListing';
+import { hasAnyRoles } from 'util/auth';
 
 import './styles.css';
-import { useParams } from 'react-router-dom';
 
 type UrlParams = {
   movieId: string;
@@ -15,55 +17,66 @@ type UrlParams = {
 const MovieDetails = () => {
   const { movieId } = useParams<UrlParams>();
 
-  const [isLoading, setIsLoading] = useState(false);
-
   const [movie, setMovie] = useState<Movie>();
 
+  const [reviews, setReviews] = useState<Review[]>([]);
+
   useEffect(() => {
-    setIsLoading(true);
-    axios
-      .get(`${BASE_URL}/movies/${movieId}`)
-      .then((response) => {
-        setMovie(response.data);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: `/movies/${movieId}/reviews`,
+      withCredentials: true,
+    };
+
+    requestBackend(config).then((response) => {
+      setMovie(response.data);
+      setReviews(response.data);
+    });
   }, [movieId]);
 
-  return (
-    <div className="movie-details-container">
-      <div className=" base-card movie-details-card">
-        <div className="row">
-          <div className="col-xl-6">
-            {isLoading ? (
-              <MovieInfoLoader />
-            ) : (
-              <>
-                <div className="img-container">
-                  <img src={movie?.imgUrl} alt={movie?.title} />
-                </div>
+  const handleInsertReview = (review: Review) => {
+    const clone = [...reviews];
+    clone.push(review);
+    setReviews(clone);
+  };
 
-                <div className="title-year-subtitle-container">
-                  <h2>{movie?.title}</h2>
-                  <h4>{movie?.year}</h4>
-                  <p>{movie?.subTitle}</p>
-                </div>
-              </>
-            )}
-          </div>
-          <div className="col-xl-6">
-            {isLoading ? (
-              <MovieDetailsLoader />
-            ) : (
+  return (
+    <>
+      <div className="movie-details-container">
+        <div className="base-card movie-details-card">
+          <div className="row">
+            <div className="col-xl-6">
+              <div className="img-container">
+                <img src={movie?.imgUrl} alt={movie?.imgUrl} />
+              </div>
+
+              <div className="title-year-subtitle-container">
+                <h2>{movie?.title}</h2>
+                <h4>{movie?.year}</h4>
+                <p>{movie?.subTitle}</p>
+              </div>
+            </div>
+
+            <div className="col-xl-6">
               <div className="description-container">
                 <p>{movie?.synopsis}</p>
               </div>
-            )}
+            </div>
           </div>
         </div>
+        <div>
+          {hasAnyRoles(['ROLE_MEMBER']) && (
+            <ReviewForm movieId={movieId} onInsertReview={handleInsertReview} />
+          )}
+          {reviews?.map((item) => (
+            <div key={item.id}>
+              <ReviewListing name={item.user.name} text={item.text} />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
+
 export default MovieDetails;
